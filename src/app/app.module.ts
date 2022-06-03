8ce6f6d08327a328cefba304910c99d5e8e45a73
DipankarDDUT <deepankar.dutta1@gmail.com>
2022-06-03 07:39:45 +0530

    3 files modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SucessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, WarningAlertComponent, SucessAlertComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+
+  
+}
